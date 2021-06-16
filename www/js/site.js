7c5a3559b05ee05e0a8ad926cf16fcd366cedd93
mattgoedde <matt_goedde@ahm.honda.com>
2021-06-16 12:12:40 -0400

    scrollspy

--- www/js/site.js
+++ www/js/site.js
@@ -12,7 +12,3 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
-
-var scrollSpy = new bootstrap.ScrollSpy(document.body, {
-    target: '#navbar'
-})

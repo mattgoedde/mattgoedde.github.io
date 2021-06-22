bc9df2f48f62cf8a7c9dfae45e38d6802f2bdffe
mattgoedde <mattgoedde99@gmail.com>
2021-06-21 22:04:34 -0400

    firebase config

--- docs/www/js/site.js
+++ docs/www/js/site.js
@@ -1,16 +1,20 @@
-var firebaseConfig = {
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
     apiKey: "AIzaSyBmgnIyfsyn1rlFUYfFuAaZXFavkKZIWKI",
     authDomain: "mattgoedde-github-io.firebaseapp.com",
+    databaseURL: "https://mattgoedde-github-io-default-rtdb.firebaseio.com",
     projectId: "mattgoedde-github-io",
     storageBucket: "mattgoedde-github-io.appspot.com",
     messagingSenderId: "908695623095",
     appId: "1:908695623095:web:5ba3e6e45e4b01c3c64432",
     measurementId: "G-YDG7RX6654"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
 const saveToFirebase = () => {
-    
+
 };
 
 
